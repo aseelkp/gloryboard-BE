f4198d8b73c160eeaa7d0795b800d4493d3bcbe4
Nahyan Sharvin <nahyansharvin@gmail.com>
2025-01-12 19:15:34 +0530

    feat: add general header image without "Participant Card" text

--- src/utils/zoneConfig.js
+++ src/utils/zoneConfig.js
@@ -6,13 +6,15 @@ export const getZoneConfig = (zone) => {
       case 'a':
         return {
           primaryColor: rgb(0.69, 0.18, 0.51), 
-          headerImagePath: './src/templates/zone_a_participant_ticket_header.png',
+          ticketHeaderImagePath: './src/templates/zone_a_participant_ticket_header.png',
+          generalHeaderImagePath: './src/templates/zone_a_header.png',
           footerText: ["Kindly submit the A-zone copy along with the following documents to the Program Office on or before 20th January.", "A copy of your SSLC Book.", "A copy of your Hall Ticket."]
         };
       case 'c':
         return {
           primaryColor: rgb(0.52, 0.45, 0.19), 
-          headerImagePath: './src/templates/zone_c_participant_ticket_header.png',
+          ticketHeaderImagePath: './src/templates/zone_c_participant_ticket_header.png',
+          generalHeaderImagePath: './src/templates/zone_c_header.png',
           footerText: ["Kindly submit the C-zone copy along with the following documents to the Program Office on or before 13th January.", "A copy of your SSLC Book.", "A copy of your Hall Ticket."]
         };
       default:
